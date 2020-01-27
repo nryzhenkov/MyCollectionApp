b8fe53be5972597bc0dca57d82be764e4a1d96be
nryzhenkov <nryzhenkov@yandex.ru>
2020-01-27 21:10:51 +0300

    Add functional of Tree

---
+++ Tree/IAcross.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollectionApp
+{
+    public interface IAcross<T>
+    {
+        public void Across(Tree<T> root);
+    }
+}
--- Tree/Tree.cs
+++ Tree/Tree.cs
@@ -2,10 +2,50 @@
 
 namespace MyCollectionApp
 {
+
     public class Tree<T>
     {
-        Tree<T> leftTree;
-        Tree<T> RightTree;
-        T value;
+        TreeItem<T> root { get; set; }
+        public TreeItem<T> currentItem { get; private set; }
+
+        public Tree(TreeItem<T> item)
+        {
+            root = currentItem = item;
+        }
+        public void GoToRightItem() 
+        {
+            if(currentItem.RightTree != null) 
+                currentItem = currentItem.RightTree;
+        }
+        public void GoToLeftItem()
+        {
+            if (currentItem.LeftTree != null)
+                currentItem = currentItem.LeftTree;
+        }
+        public void GoToRoot() 
+        {
+            currentItem = root;
+        }
+
+        public void AddRightItem(TreeItem<T> item)
+        {
+            currentItem.RightTree = item;
+        }
+
+        public void AddLeftItem(TreeItem<T> item)
+        {
+            currentItem.LeftTree = item;
+        }
+    }
+    public class TreeItem<T>
+    {
+        public TreeItem<T> LeftTree { get; set; }
+        public TreeItem<T> RightTree { get; set; }
+        public T Value { get; set; }
+
+        public TreeItem(T value)
+        {
+            Value = value;
+        }
     }
 }
